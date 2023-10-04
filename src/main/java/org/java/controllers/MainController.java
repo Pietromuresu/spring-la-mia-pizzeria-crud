edab6e39f4ef811607dd994939505b627644dcd5
Pietromuresu <muresupietro66@gmail.com>
2023-10-04 17:10:24 +0200

    add index.html and diplay menù - display an h3 if db is empty

--- src/main/java/org/java/controllers/MainController.java
+++ src/main/java/org/java/controllers/MainController.java
@@ -1,11 +1,29 @@
 package org.java.controllers;
 
+import java.util.List;
+
+import org.java.POJO.Pizza;
+import org.java.serv.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/")
 public class MainController {
-
+	
+	@Autowired
+	private PizzaService pizzaServ;
+	
 	@GetMapping
-	public String index() {
+	public String index(Model model) {
+		
+		List<Pizza> pizzas = pizzaServ.findAll();
+		
+		model.addAttribute("pizzas", pizzas);
+		
 		return "index";
 	}
 	
